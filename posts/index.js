8c2ff74fe6f094b4a78c7f513c2aa4c49b882a67
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-19 18:51:34 -0300

    sec2cap11_postservicecreation

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,17 @@
 const express= require('express')
 const app = express() 
+const { randombytes } = require('cripto')
+
+const posts = {}
 
 app.get('/posts',(req,res)=>{
-
+    res.send(posts)
 })
 
 app.post('/posts',(req,res)=>{
+    const id  = randombytes(4).toString('hex')
+})
 
+app.listen(4000,()=>{
+    console.log('listening on 4000');
 })

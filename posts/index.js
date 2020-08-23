7ee095275010a63e2767cc1032165515a0070e1b
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-23 16:25:08 -0300

    sec2cap11_postServiceCreation

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,10 @@
 const express= require('express')
 const app = express() 
 const { randombytes } = require('cripto')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
 
 const posts = {}
 
@@ -10,6 +14,11 @@ app.get('/posts',(req,res)=>{
 
 app.post('/posts',(req,res)=>{
     const id  = randombytes(4).toString('hex')
+    const{title} = req.body
+    posts[id] = {
+        id,title
+    }
+    res.status(201).send(posts[id]);
 })
 
 app.listen(4000,()=>{

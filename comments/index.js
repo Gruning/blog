8a86bbf3738d05bc1dec26dcee46dc7bb58b6271
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-13 21:26:28 -0300

    appGet

--- comments/index.js
+++ comments/index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 cosnt { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/posts/:id/comments',(req,res)=>{
+    
+})

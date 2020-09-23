ad57db3c3b4e516db55a5623221965b5f5b37eb7
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-22 23:34:27 -0300

    sec2cap13-implementingCommentsService

--- comments/index.js
+++ comments/index.js
@@ -8,12 +8,17 @@ app.use(bodyParser.json())
 const commentsByPostId ={}
 
 app.get('/posts/:id/comments',(req,res)=>{
-    
+    res.send(commentsByPostId[req.params.id ||[])
 })
 
 app.get('/posts/:id/comments',(req,res)=>{
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body
+    const comments = commentsByPostId[req.params.id || []
+    comments.push({id: commentId , content})
+    commentsByPostId[req.params.Id] = comments
+
+    res.status(201).send(comments)
 })
 app.listen(4001,()=>{
     console.log('listening on 4001');
